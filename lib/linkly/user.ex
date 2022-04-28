0adec1b38c8c6f8114ab3bc8b9c7a6c38283836d
Papa Attrams <48452451+Attrams@users.noreply.github.com>
2022-04-28 00:05:28 +0000

    add the relationship for Bookmark

--- lib/linkly/user.ex
+++ lib/linkly/user.ex
@@ -1,10 +1,12 @@
 defmodule Linkly.User do
   use Ecto.Schema
+  alias Linkly.Bookmark
 
   schema "users" do
     field(:about, :string)
     field(:email, :string)
     field(:username, :string)
+    has_many(:bookmarks, Bookmark)
 
     timestamps()
   end
